73ceae8a366143b22c61ab675828b57869c9d269
Dante <dante94009@gmail.com>
2024-08-31 02:48:57 +0300

    reformated_widget

---
+++ src/widget.py
@@ -0,0 +1,14 @@
+from src.masks import get_mask_card_number
+
+
+def mask_account_card(new_mask_number):
+    """Функция которая возвращает маску и номер карты/счета"""
+    if "Счет" in new_mask_number:
+        return f"{new_mask_number[:5]}{'**' + new_mask_number[-4:]}"
+    else:
+        return get_mask_card_number(new_mask_number)
+
+
+def get_date(new_date):
+    """Функция которая возвращает дату"""
+    return f"{new_date[8:10]}.{new_date[5:7]}.{new_date[:4]}"
