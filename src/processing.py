93cac985646de5cf59f47ea3b10013b713b52004
Dante <dante94009@gmail.com>
2024-09-11 22:35:36 +0300

    fixes anotations in processing.py and widget.py

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,16 @@
 from typing import Any
 
 
-def filter_by_state(list_of_dict:list[dict[str]], state_id: str = 'EXECUTED') -> Any:
+def filter_by_state(list_of_dict: list[dict[str, Any]], state_id: str = "EXECUTED") -> list[dict[str, Any]]:
     """Функция которая принимает на вход список словарей и выводит список по ключу"""
     new_list = []
     for i in list_of_dict:
-        if i['state'] == state_id:
+        if i["state"] == state_id:
             new_list.append(i)
     return new_list
 
-def sort_by_date(list_of_dict:list[dict[str]], reverse:bool = True):
+
+def sort_by_date(list_of_dict: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
     """Функция которая возвращает новый список, отсортированный по дате"""
-    new_sort_list = sorted(list_of_dict, key=lambda new_dict: new_dict['date'], reverse=reverse)
+    new_sort_list = sorted(list_of_dict, key=lambda new_dict: new_dict["date"], reverse=reverse)
     return new_sort_list
--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,9 @@
+from typing import Any
+
 from src.masks import get_mask_card_number
 
 
-def mask_account_card(new_mask_number: str) -> str:
+def mask_account_card(new_mask_number: Any) -> Any:
     """Функция которая возвращает маску и номер карты/счета"""
     if "Счет" in new_mask_number:
         return f"{new_mask_number[:5]}{'**' + new_mask_number[-4:]}"
@@ -9,6 +11,6 @@ def mask_account_card(new_mask_number: str) -> str:
         return get_mask_card_number(new_mask_number)
 
 
-def get_date(new_date: str) -> str:
+def get_date(new_date: Any) -> Any:
     """Функция которая возвращает дату"""
     return f"{new_date[8:10]}.{new_date[5:7]}.{new_date[:4]}"
