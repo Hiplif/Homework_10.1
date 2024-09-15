a40d8aee316d1619b3b9a07a9931212e003c5069
Dante <dante94009@gmail.com>
2024-09-15 23:46:14 +0300

    tests masks.py fixes

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -8,7 +8,8 @@ from src.masks import get_mask_card_number, get_mask_account
                          ('1472583691472583', '1472 58** **** 2583'),
                          ('3692581473692581', '3692 58** **** 2581'),
                          ('123456789454654654654', None),
-                         ('kajskjdfkjasf', None)
+                         ('kajskjdfkjasf', None),
+                         ('', None)
 ])
 
 def test_mask_card_number(number,mask):
@@ -19,7 +20,8 @@ def test_mask_card_number(number,mask):
         ('12345678945612345678', '**5678'),
         ('32165498765432165498', '**5498'),
         ('98765432165498765432', '**5432'),
-        ('123456fasf', None)
+        ('123456fasf', None),
+        ('', None)
 ])
 def test_get_mask_account(number,mask):
     assert get_mask_account(number) == mask
