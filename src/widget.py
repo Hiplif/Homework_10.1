3b16d074f38e7eb9f31e1aa2424590669ef75b77
Dante <dante94009@gmail.com>
2024-09-16 02:29:56 +0300

    tests widget finished

--- src/widget.py
+++ src/widget.py
@@ -7,10 +7,15 @@ def mask_account_card(new_mask_number: Any) -> Any:
     """Функция которая возвращает маску и номер карты/счета"""
     if "Счет" in new_mask_number:
         return f"{new_mask_number[:5]}{'**' + new_mask_number[-4:]}"
-    else:
+    elif "Maestro" or "MasterCard" or "Visa Classic" or "Visa Platinum" or "Visa Gold" in new_mask_number:
         return f'{new_mask_number[:-16]}{get_mask_card_number(new_mask_number)}'
+    else:
+        return None
 
 
 def get_date(new_date: Any) -> Any:
     """Функция которая возвращает дату"""
-    return f"{new_date[8:10]}.{new_date[5:7]}.{new_date[:4]}"
+    if len(new_date) == 26:
+       return f"{new_date[8:10]}.{new_date[5:7]}.{new_date[:4]}"
+    else:
+        return None
